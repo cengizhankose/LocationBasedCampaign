3c8d038e3cdf2e4a2bb1875a281ae01f08f486b2
Cengizhan KÖSE <kose651@gmail.com>
2020-05-28 23:35:37 +0300

    Update MapsActivity.java

--- app/src/main/java/com/example/locationbasedcampaign/ui/MapsActivity.java
+++ app/src/main/java/com/example/locationbasedcampaign/ui/MapsActivity.java
@@ -13,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import static com.example.locationbasedcampaign.GlobalVariables.chosenCategorie;
 import static com.example.locationbasedcampaign.GlobalVariables.storeList;
 import static com.example.locationbasedcampaign.GlobalVariables.chosenDistance;
 
@@ -40,8 +41,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         for (int i=0; i<storeList.size();i++){
             if(storeList.get(i).getStoreDistanceWithUser()<chosenDistance) {
-                mMap.addMarker(new MarkerOptions().position(storeList.get(i).getStoreCoordinates()).title(storeList.get(i).getStoreName()).snippet(storeList.get(i).getStoreCampaign()));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(storeList.get(i).getStoreCoordinates()));
+                for (int j = 0 ; j<storeList.get(i).getStoreCategories().size() ;j++){
+                    if (storeList.get(i).getStoreCategories().get(j).equals(chosenCategorie) || chosenCategorie.equals(null)){
+                        mMap.addMarker(new MarkerOptions().position(storeList.get(i).getStoreCoordinates()).title(storeList.get(i).getStoreName()).snippet(storeList.get(i).getStoreCampaign()));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(storeList.get(i).getStoreCoordinates()));
+                    }
+                }
             }
         }
 
