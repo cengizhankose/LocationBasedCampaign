960f4f6f1e08ed5a21bc47fd027da14aa0fb5933
Cengizhan KÖSE <kose651@gmail.com>
2020-05-28 14:52:43 +0300

    Update GlobalVariables.java

--- app/src/main/java/com/example/locationbasedcampaign/GlobalVariables.java
+++ app/src/main/java/com/example/locationbasedcampaign/GlobalVariables.java
@@ -8,4 +8,6 @@ import java.util.List;
 public class GlobalVariables extends Application {
     public static List<User>userList = new ArrayList<User>();
     public static List<Store>storeList = new ArrayList<Store>();
+    public static double chosenDistance;
+    public static String category;
 }
